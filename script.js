c8a16af8b48818d251bc7be1fea49a870ac00bdf
Charlie Antoni Yusup <charlieantoniyusup@gmail.com>
2023-05-22 19:19:37 +0700

    Adding Header

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelector(".menu-emoji").addEventListener("click", (event) => {
+
+        // console.log("clicked")
+        document.querySelector(".menu-nav").classList.toggle("clicked");
+
+    })
+
+})
